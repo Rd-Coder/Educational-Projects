3b9a7830509ee39e300cdc8c6f4b557c7a2965b8
Ruan Dias - RdCoder <rdmail.coder@gmail.com>
2021-03-16 16:03:04 -0300

    Aperfeiçoa o mecanismo de execução de comandos via subprocessos e a limpeza do terminal. Agora, para executar tais comandos, basta chamar o método privado 'CLIBuilder#execute(maxTentativas)' — que irá realizar tentativas seguidas se a execução falhar — ou 'execute()' — sem tentativas adicionais. Quanto à limpeza do terminal, foi melhorada implementando os métodos citados anteriormente e reciclando a instância de 'ProcessBuilder' que ela cria.

--- Curso-Fundamentos-de-POO-com-Java-LEAD-Fortaleza/heranca/calculadora-de-salario-liquido/src/main/java/rdcoder/edu/fundamentos_de_poo_com_java/heranca/calculadora_salario_liquido/ui/CLIBuilder.java
+++ Curso-Fundamentos-de-POO-com-Java-LEAD-Fortaleza/heranca/calculadora-de-salario-liquido/src/main/java/rdcoder/edu/fundamentos_de_poo_com_java/heranca/calculadora_salario_liquido/ui/CLIBuilder.java
@@ -1,8 +1,14 @@
-package rdcoder.edu.fundamentos_de_poo_com_java.heranca.calculadora_salario_liquido.ui;
+package rdcoder.edu.fundamentos_de_poo_com_java.heranca
+        .calculadora_salario_liquido.ui;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.Objects;
 
 import  rdcoder.edu.fundamentos_de_poo_com_java.heranca
@@ -14,11 +20,21 @@ import  rdcoder.edu.fundamentos_de_poo_com_java.heranca
  */
 public class CLIBuilder implements Runnable {
 
-    //-> ATRIBUTOS - ENTRADAS
+    //-> ATRIBUTOS
 
+    /** 
+     * O número máximo de tentativas para executar um comando via
+     * subprocesso
+     */
+    private static int MAX_TENTATIVAS_EXEC = 5;
+
+    /**
+     * O ProcessBuilder responsável por lidar com operações de I/O
+     * em {@link ProcessBuilder#inheritIO()}
+     */
     private ProcessBuilder commandListener;
-
     
+
     // -> RENDERIZAÇÃO E PROCESSAMENTO
 
     /** 
@@ -46,7 +62,7 @@ public class CLIBuilder implements Runnable {
     public void run() {
         while (true) {
             limparCLI();
-            DadosDaBonificacaoBean entradaDoUsuario = 
+            VarsBonificacaoBean entradaDoUsuario = 
                 janSolicitaValores();
             Salario totalBonificado = 
                 new SalarioLiquido(
@@ -72,11 +88,11 @@ public class CLIBuilder implements Runnable {
      * @return  um registro do tipo {@code DadosDaBonificacaoBean}
      *          contendo as variáveis coletadas
      */
-    public DadosDaBonificacaoBean janSolicitaValores() {
+    public VarsBonificacaoBean janSolicitaValores() {
         String nome, salarioBruto, bonus;
         TextBox txtBox = new TextBox();
 
-        compCabecalho();
+        comporCabecalho();
         System.out.println(
             "-> Insira as informações requeridas para o cálculo:\n");
 
@@ -86,7 +102,7 @@ public class CLIBuilder implements Runnable {
         bonus = txtBox.label("> Bônus: R$").render();
         txtBox.close();
 
-        return new DadosDaBonificacaoBean(
+        return new VarsBonificacaoBean(
             nome, 
             new SalarioBruto(Float.parseFloat(salarioBruto)),
             Float.parseFloat(bonus) 
@@ -95,7 +111,7 @@ public class CLIBuilder implements Runnable {
 
     /** Exibe o salário final resultante do cálculo */
     public void janExibeResultado(
-    final Salario resultado, final DadosDaBonificacaoBean variaveis){
+    final Salario resultado, final VarsBonificacaoBean variaveis){
         compCabecalho("Salário Final Líquido");
         System.out.printf(
             "-> Salário final para '%s:'\n\tR$ %,.2f\n",
@@ -115,51 +131,87 @@ public class CLIBuilder implements Runnable {
         confirmaListener.close();
     }
 
-    /** TODO: otimizar para reutilizar o subprocesso responsável por limpar o terminal
-     * Limpa o terminal.
+    /** 
+     * Limpa o terminal, mas se falho, imprime uma quebra de sessão.
      * 
      * @implNote esta ação é bloqueante, ou seja, pausa a thread 
      *           até que esteja concluída.
      */
     public void limparCLI() {
-        String[] comandos = 
-            (System.getProperty("os.name").contains("Windows"))
-                ? new String[]{"cmd", "/c", "cls"}
-                : new String[]{"clear"};
-
-        if (Objects.isNull(commandListener))
-            commandListener = new ProcessBuilder(comandos).inheritIO();
-        try {
-            commandListener.start().waitFor(); // bloqueante
-        } catch (IOException | InterruptedException ioOrInterrupted) {
-            ioOrInterrupted.printStackTrace();
+        String[] cmds =
+        (System.getProperty("os.name").contains("Windows"))
+            ? new String[]{"cmd", "/c", "cls"}
+            : new String[]{"clear"};
+        try { 
+            execute(MAX_TENTATIVAS_EXEC, cmds); 
+        } catch (IOException e) {
+            System.out.println(
+                "\n//////////////////////////////////////////////////////\n" 
+            );
         }
     }
 
+    /**
+     * Executa comandos via {@link ProcessBuilder#inheritIO()} e,
+     * se falho, tenta novamente até um limite de {@code maxTentativas}.
+     * 
+     * @throws  IOException 
+     *          se excedido o número máximo de tentativas
+     * @implNote esta ação é bloqueante, ou seja, pausa a thread 
+     *           até que esteja concluída.
+     */
+    private synchronized void execute(int maxTentativas,
+    String... cmds) throws IOException {
+        int tentRestantes = maxTentativas;
+        while (--tentRestantes > 0) {
+            try {
+                execute(cmds);
+                break;
+            } catch (IOException io) { continue; }
+        }
+    }
 
-    //-> BEANS
+    /**
+     * Executa comandos via {@link ProcessBuilder#inheritIO()}
+     * 
+     * @throws  IOException 
+     *          se um erro de E/S ocorrer durante a execução
+     * @implNote esta ação é bloqueante, ou seja, pausa a thread 
+     *           até que esteja concluída.
+     */
+    private synchronized void execute(String... cmds) 
+    throws IOException {
+        if (Objects.isNull(commandListener))
+            commandListener = new ProcessBuilder(cmds).inheritIO();
+        try {
+            commandListener.start().waitFor(); // bloqueante
+        } catch (InterruptedException interrupted) {}
+    }
+
+    
+    //—> BEANS
 
     /** Registro que recolhe as variáveis necessárias para o cálculo */
-    private record DadosDaBonificacaoBean(
+    private record VarsBonificacaoBean(
         String nomeDoFuncionario, Salario salarioBase, float bonus
     ) {}
 
 
-    //-> COMPONENTES EM MÉTODO
+    //—> UTILIDADES ESTÁTICAS
 
     /** 
      * Componente que exibe somente o título do programa mais uma
      * linha em branco embaixo.
      */
-    public static void compCabecalho() {
-        compCabecalho(null);
+    public static StringBuilder comporCabecalho(String titulo) {
+        return comporCabecalho(titulo, null);
     }
 
     /** 
-     * Componente que exibe o título do programa seguido de um
+     * Componente que modela o título do programa seguido de um
      * subtítulo personalizado, mais uma linha em branco embaixo.
-     * Porém, se {@code subtitulo} for {@code null}, não inclui
-     * um subtítulo ao cabeçalho. Vide formato:
+     * Porém, se {@code subtitulo} for {@code null}, o subtítulo
+     * não é incluído no cabeçalho. Vide formato:
      * 
      * <blockquote>
      * ——> [titulo] {Subtítulo} <——
@@ -167,20 +219,19 @@ public class CLIBuilder implements Runnable {
      * <blockquote>
      *  *{Subtítulo} = "— [subtitulo]"
      * </blockquote>
+     * 
      * @param   subtitulo 
      *          o subtítulo personalizado, podendo ser
      *          {@code null} para não adicionar subtítulo
+     * @return  um {@code StringBuilder} com o cabeçalho modelado
      */
-    public static void compCabecalho(String titulo, String subtitulo) {
+    public static StringBuilder comporCabecalho(String titulo, String subtitulo) {
         StringBuilder cabecalho = new StringBuilder();
-        cabecalho.append("——> ");
-        cabecalho.append(titulo);
-        if (Objects.nonNull(subtitulo)) {
-            cabecalho.append(" — ");
-            cabecalho.append(subtitulo);
-        }
+        cabecalho.append("——> " + titulo);
+        if (Objects.nonNull(subtitulo))
+            cabecalho.append(" — " + subtitulo);
         cabecalho.append(" <——");
-        System.out.println(cabecalho.toString());
+        return cabecalho;
     }
 
 
